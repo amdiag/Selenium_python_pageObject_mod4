63457ac68a4aaa3623347a161b73bbf16197d729
amdiag <amdiag@ya.ru>
2019-07-16 21:17:51 +0300

    upd done mod4.2.4

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,22 @@
-from .pages.base_page import BasePage
+import pytest
 from .pages.product_page import ProductPage
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=newYear"
 
 
-def test_guest_can_add_product_to_cart(browser):
+@pytest.mark.parametrize('link',
+                         ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_cart(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.click_button_add_to_basket()
@@ -14,3 +25,4 @@ def test_guest_can_add_product_to_cart(browser):
     page.check_product_in_basket()
     page.check_cost_of_basket()
     page.check_success_add_to_basket()
+    print("{}".format(link))
